a09cd1091bf505a7551a3583bc2081d774ecd27e
LitvinovichRoman <romainlitvinovitch@yandex.by>
2024-02-20 20:05:35 +0300

    Adde new fonts

--- MealMover/MealMover/ViewController.swift
+++ MealMover/MealMover/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20),
+        ])
+        
+        label.font = .Roboto.thin.size(of: 40)
+        label.textColor = UIColor(named: "accentOrange")
+        
+        view.backgroundColor = UIColor(named: "background")
     }
-
-
+    
+    
 }
 
