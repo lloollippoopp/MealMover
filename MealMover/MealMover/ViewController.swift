5df5d76e0e797072abf399295591098e4431607b
LitvinovichRoman <romainlitvinovitch@yandex.by>
2024-02-20 19:31:35 +0300

    Project created

---
+++ MealMover/MealMover/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealMover
+//
+//  Created by Roman Litvinovich on 20/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
